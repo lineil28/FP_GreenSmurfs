267807982623379a4832eac6d627911cc5cf5129
Neil Lin <nlin30@stuy.edu>
2022-06-09 22:12:19 -0400

    err
    trying to make it not break with non-integer inputs

--- Game.java
+++ Game.java
@@ -8,75 +8,94 @@ public class Game{
     public static void main(String[] args){
         //STUFFS VAR
         Scanner scan = new Scanner(System.in);
-        int difficulty = 0;
-        int x = 0, startX = 0;
-        int y = 0, startY = 0;
-        int action = 0;
+        String difficulty = "1211920914";
+        String x = "1211920914", startX = "1211920914";
+        String y = "1211920914", startY = "1211920914";
+        String action = "1211920914";
 
         //SELECT DIFFICULTY
-        System.out.println("Select a difficulty: \n [<1] Easy \n [1] Medium \n [>1] Hard");
-
-        try {
-            difficulty = scan.nextInt();
-        } catch (Exception e) {
-            System.out.println("Please input a valid number");
+        while(difficulty == "1211920914"){
+            System.out.println("Select a difficulty: \n [<1] Easy \n [1] Medium \n [>1] Hard");
+            difficulty = scan.next();
+            if ( ! (difficulty == "1211920914") ){
+                break;
+            }
+            else{
+                System.out.println("Please input a valid number");
+            }
         }
 
         //MAKE MAGIC
         Game minesweeper = new Game();
-        BoardTest minefield = new BoardTest(difficulty);
+        BoardTest minefield = new BoardTest(Integer.parseInt(difficulty));
 
         //START SQUARE
-        System.out.println("Pick an x coordinate");
+        while (startX == "1211920914"){
+            System.out.println("Choose an starting x coordinate");
+            startX = scan.next();
+            if ( ! (startX == "1211920914") ){
+                break;
+            }
+            else{
+                System.out.println("Please input a number within the board");
+            }
+        }
 
-        try {
-            startX = scan.nextInt();
-        } catch (Exception e) {
-            System.out.println("Please input a number within the board");
-        }
-        System.out.println("Pick a y coordinate");
-        try {
-            startY = scan.nextInt();
-        } catch (Exception e) {
-            System.out.println("Please input a number within the board");
-        }
-        minefield.populate(startY, startX);
+        while(startY == "1211920914"){
+            System.out.println("Choose a starting y coordinate");
+            startY = scan.next();
+            if ( ! (startY == "1211920914") ){
+                break;
+            }
+            else{
+                System.out.println("Please input a number within the board");
+            }
+          }
+
+        minefield.populate(Integer.parseInt(startY), Integer.parseInt(startX));
         System.out.println(minefield.revealString());
 
         //PLAY
         while (!minefield.gameOver){
 
-            System.out.println("Pick an x coordinate");
-
-            try {
-                x = scan.nextInt();
-                //System.out.println(x);
-            } catch (Exception e) {
-                //TODO: handle exception
-                System.out.println("Please input a number within the board");
+            while(x == "1211920914"){
+                System.out.println("Choose an x coordinate");
+                x = scan.next();
+                if ( ! (x == "1211920914") ){
+                    break;
+                }
+                else{
+                    System.out.println("Please input a number within the board");
+                }
             }
-            System.out.println("Pick a y coordinate");
-            try {
-                y = scan.nextInt();
-                //System.out.println(y);
-            } catch (Exception e) {
-                //TODO: handle exception
-                System.out.println("Please input a number within the board");
+
+            while(y == "1211920914"){
+                System.out.println("Choose an y coordinate");
+                y = scan.next();
+                if ( ! (y == "1211920914") ){
+                    break;
+                }
+                else{
+                    System.out.println("Please input a number within the board");
+                }
             }
 
             //ACTION - EXPLORE OR FLAG
-            System.out.println("Actions: \n [<=1] Explore \n [>1] Flag/Unflag");
-
-            try {
-                action = scan.nextInt();
-            } catch (Exception e) {
-                System.out.println("Please input a valid number");
+            while (action == "1211920914"){
+                System.out.println("Actions: \n [<=1] Explore \n [>1] Flag/Unflag");
+                action = scan.next();
+                if ( ! (y == "1211920914") ){
+                    break;
+                }
+                else{
+                    System.out.println("Please input a valid number");
+                }
             }
 
             //BOARD MAGIC
-            if (difficulty < 1 && action <= 1){
-              if (x > 0 && y > 0 && x <= 8 && y <= 8){
-                  minefield.explore(y,x);
+            if (Integer.parseInt(difficulty) < 1 && Integer.parseInt(action) <= 1){
+              if (Integer.parseInt(x) > 0 && Integer.parseInt(y) > 0 && Integer.parseInt(x) <= 8 && Integer.parseInt(y) <= 8){
+                  minefield.explore(Integer.parseInt(y), Integer.parseInt(x));
 
                   System.out.println(minefield.revealString());
               }
@@ -84,17 +103,17 @@ public class Game{
                 System.out.println("Coordinates not on board.");
               }
             }
-            else if (difficulty < 1 && action > 1){
-              if (x > 0 && y > 0 && x <= 8 && y <= 8){
-                minefield.setFlag(y, x);
+            else if (Integer.parseInt(difficulty) < 1 && Integer.parseInt(action) > 1){
+              if (Integer.parseInt(x) > 0 && Integer.parseInt(y) > 0 && Integer.parseInt(x) <= 8 && Integer.parseInt(y) <= 8){
+                minefield.setFlag(Integer.parseInt(y), Integer.parseInt(x));
 
                 System.out.println(minefield.revealString());
               }
             }
 
-            if (difficulty == 1 && action <= 1){
-              if (x > 0 && y > 0 && x <= 16 && y <= 16){
-                  minefield.explore(y,x);
+            if (Integer.parseInt(difficulty) == 1 && Integer.parseInt(action) <= 1){
+              if (Integer.parseInt(x) > 0 && Integer.parseInt(y) > 0 && Integer.parseInt(x) <= 16 && Integer.parseInt(y) <= 16){
+                  minefield.explore(Integer.parseInt(y), Integer.parseInt(x));
 
                   System.out.println(minefield.revealString());
               }
@@ -102,17 +121,17 @@ public class Game{
                 System.out.println("Coordinates not on board.");
               }
             }
-            else if (difficulty == 1 && action > 1){
-              if (x > 0 && y > 0 && x <= 16 && y <= 16){
-                minefield.setFlag(y, x);
+            else if (Integer.parseInt(difficulty) == 1 && Integer.parseInt(action) > 1){
+              if (Integer.parseInt(x) > 0 && Integer.parseInt(y) > 0 && Integer.parseInt(x) <= 16 && Integer.parseInt(y) <= 16){
+                minefield.setFlag(Integer.parseInt(y), Integer.parseInt(x));
 
                 System.out.println(minefield.revealString());
               }
             }
 
-            if (difficulty > 1 && action <= 1){
-              if (x > 0 && y > 0 && x <= 16 && y <= 32){
-                  minefield.explore(y,x);
+            if (Integer.parseInt(difficulty) > 1 && Integer.parseInt(action) <= 1){
+              if (Integer.parseInt(x) > 0 && Integer.parseInt(y) > 0 && Integer.parseInt(x) <= 16 && Integer.parseInt(y) <= 32){
+                  minefield.explore(Integer.parseInt(y), Integer.parseInt(x));
 
                   System.out.println(minefield.revealString());
               }
@@ -120,19 +139,20 @@ public class Game{
                 System.out.println("Coordinates not on board.");
               }
             }
-            else if (difficulty > 1 && action > 1){
-              if (x > 0 && y > 0 && x <= 16 && y <= 32){
-                minefield.setFlag(y,x);
+            else if (Integer.parseInt(difficulty) > 1 && Integer.parseInt(action) > 1){
+              if (Integer.parseInt(x) > 0 && Integer.parseInt(y) > 0 && Integer.parseInt(x) <= 16 && Integer.parseInt(y) <= 32){
+                minefield.setFlag(Integer.parseInt(y), Integer.parseInt(x));
 
                 System.out.println(minefield.revealString());
               }
             }
 
-            
+
 
         }
 
         System.out.println(minefield.revealString());
+        System.out.println(minefield.toString());
         System.out.println("You Hit a Bomb! \n Game Over.");
 
     }
